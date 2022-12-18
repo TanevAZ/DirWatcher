cbd30295301a96445fd37df50c8fda23187fb89d
Tanev <66444552+TanevAZ@users.noreply.github.com>
2022-12-18 18:05:40 +0100

    DirWatcher

---
+++ DirWatcher/Py/main.py
@@ -0,0 +1,27 @@
+import os
+import time
+import datetime
+
+dir = 'C:\\Users\\kiiza\\Documents\\AllTools\\PyTools\\FileMonitor'
+logpath = "C:\\Users\\kiiza\\Documents\\AllTools\\PyTools\\FileMonitor\\filelog.txt"
+
+log = open(logpath, "a")
+
+editTimes = {}
+
+while True:
+  for filename in os.listdir(dir):
+    timecount = os.path.getmtime(os.path.join(dir, filename))
+
+    if timecount > editTimes.get(filename, 0):
+      print(f'[{time.ctime(timecount)}] : {filename} was modified.')
+
+      now = datetime.datetime.now()
+
+      formattedDate = now.strftime('%d/%m/%Y')
+
+      log.write(f'[{time.ctime(timecount)} | {formattedDate}] : {filename} was modified.\n')
+
+      editTimes[filename] = timecount
+
+  time.sleep(1)
